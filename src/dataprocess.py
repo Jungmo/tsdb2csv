e1072538dce3b41431a4de4863e1549ca4345fcb
Jungmo <ajm100@ajou.ac.kr>
2016-08-28 17:00:03 +0900

    Add tag

--- src/dataprocess.py
+++ src/dataprocess.py
@@ -6,10 +6,13 @@ import func
 class metric:
     def __init__(self, url):
         response = urllib.urlopen(url);
-        resp_json = response.read()
+        resp_json = response.readline()
         resp_json = resp_json[1:-1]
         json_data = json.loads(resp_json)
 
+        print json_data
         self.metric_name = json_data["metric"]
         self.value = json_data["dps"]
         self.value = func.dict_sort(self.value)
+
+        print "Metric : " + self.metric_name +" is loaded."
--- src/main.py
+++ src/main.py
@@ -7,31 +7,33 @@ import dataprocess as dp
 import easygui
 
 #TODO:get metric name on GUI from HTTP API
-metric = ["wearable.hr", "wearable.acc_x", "wearable.acc_y", "wearable.acc_z"]
-
+# http://serverurl/api/suggest?type=metrics&max=20
+#metric = ["wearable.hr", "wearable.acc_x", "wearable.acc_y", "wearable.acc_z"]
+metric = "0828still"
+tag = ["hr", "x", "y", "z"]
 #FIXME:Sometimes easygui doesn't work well. Use tkinter.
 # http://stackoverflow.com/questions/29783090/how-to-use-the-calendar-module-with-tkinter
-start_date = easygui.enterbox('start_date (YYYY/MM/DD-hh:mm:ss) : ')
-end_date = easygui.enterbox('end_date (YYYY/MM/DD-hh:mm:ss) : ')
+#start_date = easygui.enterbox('start_date (YYYY/MM/DD-hh:mm:ss) : ')
+#end_date = easygui.enterbox('end_date (YYYY/MM/DD-hh:mm:ss) : ')
 
-#start_date = "2016/08/05-16:23:00"
-#end_date = "2016/08/05-16:30:00"
+start_date = "1d-ago" #""2016/08/05-16:23:00"
+end_date = "1s-ago" #"2016/08/05-16:30:00"
 
-server_url = "http://10.0.1.43:4242/" # OpenTSDB Server Address
-query_url = "api/query?start="+start_date+"&end="+end_date+"&m=sum:"
+server_url = "http://202.30.29.209:14242/" # OpenTSDB Server Address
+query_url = "api/query?start="+start_date+"&end="+end_date+"&m=sum:"+metric
 
 url = []
 
-for i in range(0,len(metric)):
-    url.append(server_url+query_url+metric[i])
+for i in range(0,len(tag)):
+    url.append(server_url+query_url+"{host="+tag[i]+"}")
 
 hr = dp.metric(url[0])
 acc_x = dp.metric(url[1])
-acc_y = dp.metric(url[1])
-acc_z = dp.metric(url[1])
+acc_y = dp.metric(url[2])
+acc_z = dp.metric(url[3])
 
 
-print "Set minimum timestamp and maximum timestamp"
+print "Set minimum and maximum timestamp"
 mints = min(min(hr.value.keys()),
             min(acc_x.value.keys()),
             min(acc_y.value.keys()),
@@ -49,7 +51,7 @@ print "mints : ", mints,"maxts : ",maxts
 f = open('../output/file.csv','wb')
 
 writer = csv.writer(f, delimiter=',')
-writer.writerow(['unix_ts','real_ts']+metric)
+writer.writerow(['unix_ts','real_ts']+tag)
 
 #TODO: Handle 'invalild key' situation
 for i in range(int(mints), int(maxts)):
@@ -57,4 +59,4 @@ for i in range(int(mints), int(maxts)):
     try:
         writer.writerow([ts, func.unixtime2realtime(ts), hr.value[ts], acc_x.value[ts], acc_y.value[ts], acc_z.value[ts]])
     except KeyError:
-        print "error"
+        print ts ,"error"
